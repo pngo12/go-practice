04d5a7efb6daf215b2f22aea166719b3ac6767d3
pngo12 <pngo12@gmail.com>
2018-11-30 16:19:31 -0800

    first commit golang

---
+++ basics/hello/hello.go
@@ -0,0 +1,13 @@
+package main
+// fmt gives access to certain methods
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+// To run file:
+// go run <Name of File>
+
+// To compile:
+// go build <Name of File>
---
+++ basics/variables/variables.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Phillip Ngo"
+	fmt.Println(name)
+}
