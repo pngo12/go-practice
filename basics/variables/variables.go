9118857ebb5fbb6e26a6a36b0598abfe4e3404fd
pngo12 <pngo12@gmail.com>
2018-11-30 16:29:35 -0800

    variables

--- basics/variables/variables.go
+++ basics/variables/variables.go
@@ -3,6 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	var name string = "Phillip Ngo"
-	fmt.Println(name)
+	// var name string = "Phillip Ngo"
+	// var age int = 100
+	// Same as JS es6 Const
+	const isCool = true
+
+	// Shorthand variables
+	// No need to declare type
+	// It is inferred
+	//  name := "Phillip"
+	 size := 5.0
+	 
+	 // Combine variable declarations
+	 // Similar to JS destructuring ish
+	 name, email := "Phillip", "pngo12@gmail.com"
+
+
+	fmt.Println(name + " " + email)
+	fmt.Printf("%T\n", size)
 }
